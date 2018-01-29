b6ee4ab2545b357565ae7eb127a495d6e1d05408
Joseph Weidinger <jsphweid@gmail.com>
2018-01-28 16:45:01 -0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+bucket_name=$(<~/.aws/main_bucket_name)
+cloudfront_id=$(<~/.aws/main_cloudfront_id)
+
+aws s3 sync ./dist/ s3://${bucket_name}/playEquation --delete
+aws cloudfront create-invalidation --distribution-id ${cloudfront_id} --paths "/*"
