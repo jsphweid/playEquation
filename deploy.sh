52c587cd7b24573155fa0a63413e219fafac03a6
Joseph Weidinger <jsphweid@gmail.com>
2018-01-29 14:31:29 -0800

    fix deploy process, it goes to build/ not dist/

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 bucket_name=$(<~/.aws/main_bucket_name)
 cloudfront_id=$(<~/.aws/main_cloudfront_id)
 
-aws s3 sync ./dist/ s3://${bucket_name}/playEquation --delete
+aws s3 sync ./build/ s3://${bucket_name}/playEquation --delete
 aws cloudfront create-invalidation --distribution-id ${cloudfront_id} --paths "/*"
